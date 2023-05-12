ce929d71ce523ef5377e7a7eae58a0ef51bf8897
Alan Liang <symb@olic.link>
2023-05-12 10:19:30 +0800

    chore: generate debug symbol file for address with offset

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -62,8 +62,8 @@
 #define MAXVA (1L << (9 + 9 + 9 + 12 - 1))
 #define SPLIT    0x3f00000000L
 #define KERNPHY  (PHYBASE)
-// #define KERNBASE (SPLIT)
-#define KERNBASE (PHYBASE)
+#define KERNBASE (SPLIT)
+// #define KERNBASE (PHYBASE)
 #define KERNSTOP (KERNBASE + 0x40000000L)
 #define KERN_PHYBASE (KERNBASE)
 #define KERN_PHYSTOP (KERN_PHYBASE + PHYSIZE)
